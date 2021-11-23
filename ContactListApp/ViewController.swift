f1559dd7799fb33021482650be9efc24cd8b8f7e
Ahmed Fayek <ahmed.fayek@vodafone.com>
2021-11-23 19:39:08 +0200

    open connection to sqlite db

--- ContactListApp/ViewController.swift
+++ ContactListApp/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import SQLite3
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        openDatabase()
     }
-
-
+    
+    func openDatabase() -> OpaquePointer? {
+        var dbOpaquePointer: OpaquePointer?
+        let fileUrl = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("Contacts.sqlite")
+        if sqlite3_open(fileUrl?.path, &dbOpaquePointer) == SQLITE_OK {
+            print("successfully opened connection to db")
+            return dbOpaquePointer
+        }else {
+            print("unable to open db connection")
+            return nil
+        }
+    }
 }
 
